c12bb89212bc4f67b0bf0d46410f4e559c03d6ab
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-08-31 20:57:49 +0100

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/easy/easyUI5App/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.easy.easyUI5App.controller.MainView", {});
+});
